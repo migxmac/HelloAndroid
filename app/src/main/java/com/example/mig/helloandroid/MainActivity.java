909c9b811ed0599886bac66b36d635fd2560cf38
Miguel Macias <migxmac@gmail.com>
2017-12-12 22:44:57 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mig/helloandroid/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.mig.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    /**
+     * <p>
+     * Method called when activity is created
+     * </p>
+     * <p>
+     *     Exciting stuff is happening here
+     * </p>
+     * @param savedInstanceState the saved instance state
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
